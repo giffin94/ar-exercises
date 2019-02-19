7fbccefa544ee0837eb35b0d2d7c18dc4e71d4f1
Aedan <aedan.giffin@gmail.com>
2019-02-19 20:58:28 +0000

    Ex 5 done

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+@average_revenue = Store.average(:annual_revenue)
+puts "#{@total_revenue}"
+puts "#{@average_revenue}"
+
+@millionaire_companies = Store.where(annual_revenue: 1000000..Float::INFINITY).count
+puts @millionaire_companies;
